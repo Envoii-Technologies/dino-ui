a93ea89f0f381a992be17b4f5defc3e9a42cae37
FRickReich <frickreich@gmail.com>
2023-10-18 13:03:50 +0200

    feat(buttongroup): add compact mode to Component

--- src/components/ButtonGroup/ButtonGroup.jsx
+++ src/components/ButtonGroup/ButtonGroup.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ButtonGroup.scss'
 
-export const ButtonGroup = ({ className, children, ...props }) =>
+export const ButtonGroup = ({ className, children, compact, fluid, ...props }) =>
 {
 	return (
-		<div className={`ButtonGroup ${ className !== undefined ? className : "" }`}>
+		<div className={`ButtonGroup ${ className !== undefined ? className : "" } ${compact ? 'compact' : ''} ${fluid ? 'fluid' : ''}`}>
 			{ children }
 		</div>
 	)
@@ -17,9 +17,13 @@ ButtonGroup.propTypes =
 	 * Custom class name of Component
 	 */
 	className: PropTypes.string,
+	compact: PropTypes.bool,
+	fluid: PropTypes.bool,
 };
 
 ButtonGroup.defaultProps =
 {
 	className: undefined,
+	compact: false,
+	fluid: false,
 };
