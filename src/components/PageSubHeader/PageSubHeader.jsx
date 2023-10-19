d72c1e9a1de6851f0e1c10b1327a2bd0c0a7e634
FRickReich <frickreich@gmail.com>
2023-10-19 15:45:48 +0200

    feat: add PageSubHeader component

---
+++ src/components/PageSubHeader/PageSubHeader.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './PageSubHeader.scss'
+
+export const PageSubHeader = ({ className,  children, ...props }) =>
+{
+	return (
+		<div className={`PageSubHeader ${ className !== undefined ? className : "" }`}>
+			{ children }
+		</div>
+	)
+}
+
+PageSubHeader.propTypes =
+{
+	/**
+	 * Custom class name of Component
+	 */
+	className: PropTypes.string,
+};
+
+PageSubHeader.defaultProps =
+{
+	className: undefined,
+};
---
+++ src/data/mockUser.js
@@ -0,0 +1,10 @@
+export default {
+    "tenant": "acme",
+    "id": "afc2125d-0a51-41ae-b46f-bd1e3e797797",
+    "username": "admin",
+    "email": "admin@example.com",
+    "emailVerified": true,
+    "firstname": "Admin",
+    "lastname": "User",
+    "roles": ["app_admin"]
+};
