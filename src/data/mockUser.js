73786531a837b6bb1025dff0bd41d788235d9606
FRickReich <frickreich@gmail.com>
2023-10-27 08:16:31 +0200

    fix(sidebar): change mobile menu layout

--- src/data/mockUser.js
+++ src/data/mockUser.js
@@ -1,5 +1,11 @@
 export default {
-    "tenant": "acme",
+    "tenant": {
+        "_id": "652eaf862429a2004a321be5",
+        "short": "acme",
+        "name": "ACME Corporation",
+        "logo": "",
+        "__v": 0
+    },
     "id": "afc2125d-0a51-41ae-b46f-bd1e3e797797",
     "username": "admin",
     "email": "admin@example.com",
