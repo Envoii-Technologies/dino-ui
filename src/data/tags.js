d91891c3adecf7a0123fa31fe903cac0888d4e4d
FRickReich <frickreich@gmail.com>
2023-10-30 14:30:20 +0100

    fix(selectbox): fix creatable option

--- src/data/tags.js
+++ src/data/tags.js
@@ -1,7 +1,7 @@
 export default [
-    { value: 'document', label: 'document' },
-    { value: 'produktion', label: 'produktion' },
-    { value: 'externer request', label: 'externer request' },
-    { value: 'wartung', label: 'wartung' },
-    { value: 'montageanleitung', label: 'montageanleitung' },    
+    { value: '5449593d98c0e61fdc8362d1', label: 'document' },
+    { value: '5449593d98c0e61fdc8362d2', label: 'produktion' },
+    { value: '5449593d98c0e61fdc8362d3', label: 'externer request' },
+    { value: '5449593d98c0e61fdc8362d4', label: 'wartung' },
+    { value: '5449593d98c0e61fdc8362d5', label: 'montageanleitung' },    
 ];
