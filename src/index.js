74389b2b20e4a17f1ee4f11b14f5c551e0261970
FRickReich <frickreich@gmail.com>
2023-11-14 13:02:26 +0100

    feat: add TextEditor component

--- src/index.js
+++ src/index.js
@@ -57,3 +57,4 @@ export { LockView } from './components/LockView/index.js';
 export { TableView } from './components/TableView/index.js';
 export { ImageEditor } from './components/ImageEditor/index.js';
 export { MediaPreview } from './components/MediaPreview/index.js';
+export { TextEditor } from './components/TextEditor/index.js';
